5c58e26b5f112ed42d937b6b74b03bf88770bc6a
Abhinav Sinha <abhinavsinha001@gmail.com>
2017-09-08 18:48:19 +0530

    Added qps metric endpoint

--- server.go
+++ server.go
@@ -3,13 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "github.com/paulbellamy/ratecounter"
+    "time"
 )
 
+var counter = ratecounter.NewRateCounter(1 * time.Second)
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    counter.Incr(1)
+}
+
+func metricsQpsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Qps %d!", counter.Rate())
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/application", handler)
+    http.HandleFunc("/metrics/qps", metricsQpsHandler)
     http.ListenAndServe(":8080", nil)
 }
